405b7f6d64483a327745f2ae41eb35dd17c01552
Arturo Rivas <arturo.rivas@icloud.com>
2023-03-17 19:51:13 +0100

    Chat stream test

--- Tests/SwiftOpenAITests/SwiftOpenAITests.swift
+++ Tests/SwiftOpenAITests/SwiftOpenAITests.swift
@@ -2,13 +2,22 @@ import XCTest
 @testable import SwiftOpenAI
 
 final class SwiftOpenAITests: XCTestCase {
-    private var apiKey = ProcessInfo.processInfo.environment["API_KEY"]!
+    let openAI = SwiftOpenAI(ProcessInfo.processInfo.environment["API_KEY"]!)
     
-    func testStreamChat() async throws {
-        let openAI = SwiftOpenAI(apiKey)
+    override func setUp() {
+        openAI.clearHistory()
+    }
+
+    func testChat() async throws {
         _ = try await openAI.chat("Me llamo Arturo Rivas")
         _ = try await openAI.chat("¿Cuál es mi nombre?")
         openAI.clearHistory()
         _ = try await openAI.chat("¿Cuál es mi nombre?")
+    }
+    
+    func testChatStream() async throws {
+        for try await result in try await openAI.chatStream("¿Qué tal estás?") {
+            print(result)
+        }
     }
 }
